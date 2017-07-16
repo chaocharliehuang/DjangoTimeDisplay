9cb995a1258932a06458ead9ceaa3c5cefd35c6c
Chao Charlie Huang <chaocharliehuang@gmail.com>
2017-07-16 10:12:44 -0700

    using django.utils' timezone insteadf of time's localtime

--- apps/time_display/views.py
+++ apps/time_display/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect, HttpResponse
-from time import localtime, strftime
+# from time import localtime, strftime
+from django.utils import timezone
 
 # Create your views here.
 def index(request):
+    now = timezone.now()
     context = {
-        "date": strftime("%b %d, %Y", localtime()),
-        "time": strftime("%I:%M %p", localtime())
+        "date": now.strftime("%b %d, %Y"),
+        "time": now.strftime("%I:%M %p")
     }
     return render(request, 'time_display/index.html', context)
